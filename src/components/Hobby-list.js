8621175fae1e17c8ea51ddbe5df5e4a359d6e83b
LizbethFP <lizbeth.fp@gmail.com>
2018-03-14 15:14:45 -0500

    retirando pictitle, sí se muestra listas y header

--- src/components/Hobby-list.js
+++ src/components/Hobby-list.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HobbyListUl from './Hobby-list-ul';
 import HobbyListLi from './Hobby-list-li';
+// import PicTitle from './pic-title';
 
 const HobbyList = () => (
   <section>
+    {/* <PicTitle/> */}
     <HobbyListUl/>
   </section>
 )
---
+++ src/components/pic-title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Chick from './assets/images/chick.jpg';
+
+const PicTitle = () => (
+  // <div>
+  //   <h3>Chick</h3>
+  //   <img src={Chick} alt={Chick}/>
+  // </div>
+)
+
+export default PicTitle;
